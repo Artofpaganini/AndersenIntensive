f77014041ffb5aee780130e4d84a51800daa1f9d
Victor Dobrov <VDobrov@alfabank.kz>
2022-04-12 20:28:45 +0300

    commit

--- app/src/main/java/com/example/myapplication/AFragment.kt
+++ app/src/main/java/com/example/myapplication/AFragment.kt
@@ -1,8 +1,16 @@
 package com.example.myapplication
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class AFragment : Fragment(R.layout.fragment_a) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 
     companion object {
         const val FRAGMENT_A_TAG = "FRAGMENT_A_TAG"
